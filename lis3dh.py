d42437066e8b95196716b12dd7dcc80c559e1e55
Yuki-Mori <yuki7291@pluslab.org>
2018-09-27 20:33:45 +0900

    High regなんちゃらモードにして12bitsの精度で取得できるようになった。

--- lis3dh.py
+++ lis3dh.py
@@ -23,7 +23,8 @@ class Lis3dh(object):
             'Y_H' : 0x2b,
             'Z_L' : 0x2c,
             'Z_H' : 0x2d,
-            'CTRL_REG1' : 0x20
+            'CTRL_REG1' : 0x20,
+            'CTRL_REG3' : 0x23
         }
         self._default_value = {
             'x' : 0.0,
@@ -33,7 +34,12 @@ class Lis3dh(object):
         self._bus.write_i2c_block_data(
             self._addr,
             self._register['CTRL_REG1'],
-            [0x7f])
+            #[0x7f])
+            [0x77])
+        self._bus.write_i2c_block_data(
+            self._addr,
+            self._register['CTRL_REG3'],
+            [0b00001000])
 
     def read(self, reg_name):
         if reg_name not in self._register:
@@ -59,21 +65,24 @@ class Lis3dh(object):
     def get_acceleration(self):
         l = self.read('X_L')
         h = self.read('X_H')
-        #x = (h << 8 | l) >> 4
-        x = (l << 8 | h)
-        x = self.byte2int(x) / 1024.0 * 980.0 - self._default_value['x']
+        x = (h << 8 | l) >> 4
+        #x = (l << 8 | h)
+        x = self.byte2int(x) / 1024.0
+        #x = self.byte2int(x) / 1024.0 * 980.0 - self._default_value['x']
 
         l = self.read('Y_L')
         h = self.read('Y_H')
-        #y = (h << 8 | l) >> 4
-        y = (l << 8 | h)
-        y = self.byte2int(y) / 1024.0 * 980.0 - self._default_value['y']
+        y = (h << 8 | l) >> 4
+        #y = (l << 8 | h)
+        y = self.byte2int(y) / 1024.0
+        #y = self.byte2int(y) / 1024.0 * 980.0 - self._default_value['y']
 
         l = self.read('Z_L')
         h = self.read('Z_H')
-        #z = (h << 8 | l) >> 4
-        z = (l << 8 | h)
-        z = self.byte2int(z) / 1024.0 * 980.0 - self._default_value['z']
+        z = (h << 8 | l) >> 4
+        #z = (l << 8 | h)
+        z = self.byte2int(z) / 1024.0
+        #z = self.byte2int(z) / 1024.0 * 980.0 - self._default_value['z']
 
         return (x, y, z)
     
@@ -92,8 +101,8 @@ class Lis3dh(object):
         self._default_value['z'] = data[2]
 
     def byte2int(self, value):
-        #return -(value & 0b100000000000) | (value & 0b011111111111)
-        return -(value & 0b10000000) | (value & 0b01111111)
+        return -(value & 0b100000000000) | (value & 0b011111111111)
+        #return -(value & 0b10000000) | (value & 0b01111111)
 
 def old_main():
     print('Hello, world!')
@@ -110,6 +119,7 @@ def main():
         while True:
             a = sen.get_acceleration()
             print(a)
+            #print(bin(a[3]|0b100000000),bin(a[4]|0b100000000))
             sleep(1)
     except:
         pass
